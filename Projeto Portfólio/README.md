2fce92ccadd9d32b99a5f3e411aa540ff29a9cf2
Naiara-Oliveira <89230677+Naiara-Oliveira@users.noreply.github.com>
2021-09-15 14:08:47 -0400

    Add files via upload

---
+++ Projeto Portfólio/README.md
@@ -0,0 +1,14 @@
+# portifolio
+ Portifólio Do Desenvolvedor
+
+ ## tarefas
+
+ O controle dessa tarefas serão feitas no GitHub
+
+## Icones
+
+- :package:nova funcionalidade
+- :up:atualização
+- :beatle:correção de bug
+- :checkered_flag:release
+
---
+++ Projeto Portfólio/script.js
@@ -0,0 +1,28 @@
+var hamburguer = document.querySelector(".hamburguer")
+hamburguer.addEventListener("click", function() {
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+document.querySelector("#qtd").addEventListener("change" , atualizarPreco)
+document.querySelector("#js").addEventListener("change" , atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change" , atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change" , atualizarPreco)
+document.querySelector("#prazo").addEventListener("change" ,function(){
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = "Prazo: " + prazo + " semanas"
+atualizarPreco()
+
+})
+function atualizarPreco(){
+    const qtd = document.querySelector("#qtd").value
+    const temjs = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+  
+    let preco = qtd * 100;
+    if (temjs) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+   
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
